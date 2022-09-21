2a2c2c6dd1c4b2f35257290baf10d9feafa4edfc
Jonathan Sánchez <jonulisanpar@gmail.com>
2022-09-21 07:59:32 -0700

    aplicación Navegacion
    - Navegación basica con segue

---
+++ Navegacion/Navegacion/TercerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TercerViewController.swift
+//  Navegacion
+//
+//  Created by contrasena12345 on 9/21/22.
+//
+
+import UIKit
+
+class TercerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func regresar(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Navegacion/Navegacion/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Navegacion
+//
+//  Created by contrasena12345 on 9/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func regresarInicio(segue: UIStoryboardSegue) {
+        // DEJAR VACIO
+    }
+
+}
+
