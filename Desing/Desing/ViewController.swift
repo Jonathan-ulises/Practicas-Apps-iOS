3273113d05feb974a9a0ec7117c086d0c1eb55a6
Jonathan Sánchez <jonulisanpar@gmail.com>
2022-09-30 07:48:08 -0700

    Diseño de apps - Constrains
    Ejercicio inicial de diseño de apps con constrains.

---
+++ Desing/Desing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Desing
+//
+//  Created by contrasena12345 on 9/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
